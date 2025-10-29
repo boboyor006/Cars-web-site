dbc63443ee3cad129bd34eaee6459b029dea6822
Boboyor Po'latov <162977738+boboyor006@users.noreply.github.com>
2025-10-29 10:17:43 +0500

    web saytni boshlaymiz

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+<div className="bg-blue-500 text-white p-4 rounded">
+  Tailwind bilan stil
+</div>    </>
+  )
+}
+
+export default App
